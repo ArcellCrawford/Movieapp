0bd18ca3c3c0a6ba2b523860e0f372fb2c0032dd
TXT-77 <txt.hackathon@TXTs-MacBook-Pro-88.local>
2023-07-11 15:05:05 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+//import './src/styles.css';
+
+export default function App() {
+  return (
+    <View >
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
